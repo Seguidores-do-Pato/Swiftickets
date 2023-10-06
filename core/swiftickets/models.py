a133dcae6c93eab6b6606bb65cb02a35014ea154
Emauri-Neto <emaurigomesneto@gmail.com>
2023-10-05 22:13:58 -0300

    add models

--- core/swiftickets/models.py
+++ core/swiftickets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstName = models.CharField(max_length=50)
+    secondName = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(max_length=200, upload_to=img_path)
+    birthday = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
